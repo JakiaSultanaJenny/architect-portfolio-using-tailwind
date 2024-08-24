cb328f2f6d5bf3003d1061b937d62c7f869d03c2
JakiaSultana <jenny15cu@gmail.com>
2024-08-25 01:38:41 +0900

    Added the we design thoughtfull, liavle spaces section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+<script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            background_cl: '#C30000',
+          }
+        }
+      }
+    }
+  </script>
